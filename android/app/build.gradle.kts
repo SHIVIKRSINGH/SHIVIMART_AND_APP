d78a080947a9462c98555bb53d8c4cab781aaafa
SHIVIKRSINGH <shivendrasoftgen@gmail.com>
2026-05-28 21:59:07 +0530

    upd

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,44 +1,91 @@
+import java.util.Properties
+import java.io.FileInputStream
+
 plugins {
-    id("com.android.application")
-    id("kotlin-android")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
-    id("dev.flutter.flutter-gradle-plugin")
+id("com.android.application")
+id("kotlin-android")
+id("dev.flutter.flutter-gradle-plugin")
+}
+
+val keystoreProperties = Properties()
+val keystorePropertiesFile = rootProject.file("key.properties")
+
+if (keystorePropertiesFile.exists()) {
+keystoreProperties.load(
+FileInputStream(keystorePropertiesFile)
+)
 }
 
 android {
-    namespace = "com.softgensys.shivimart"
-    compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
 
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
+```
+namespace = "com.softgensys.shivimart"
 
-    kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_17.toString()
-    }
+compileSdk = flutter.compileSdkVersion
 
-    defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.softgensys.shivimart"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
-        versionCode = flutter.versionCode
-        versionName = flutter.versionName
-    }
+ndkVersion = flutter.ndkVersion
 
-    buildTypes {
-        release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
-        }
+compileOptions {
+
+    sourceCompatibility = JavaVersion.VERSION_17
+
+    targetCompatibility = JavaVersion.VERSION_17
+}
+
+kotlinOptions {
+
+    jvmTarget = JavaVersion.VERSION_17.toString()
+}
+
+signingConfigs {
+
+    create("release") {
+
+        keyAlias =
+            keystoreProperties["keyAlias"] as String
+
+        keyPassword =
+            keystoreProperties["keyPassword"] as String
+
+        storeFile =
+            file(
+                keystoreProperties["storeFile"] as String
+            )
+
+        storePassword =
+            keystoreProperties["storePassword"] as String
     }
 }
 
+defaultConfig {
+
+    applicationId = "com.softgensys.shivimart"
+
+    minSdk = flutter.minSdkVersion
+
+    targetSdk = flutter.targetSdkVersion
+
+    versionCode = flutter.versionCode
+
+    versionName = flutter.versionName
+}
+
+buildTypes {
+
+    release {
+
+        signingConfig =
+            signingConfigs.getByName("release")
+    }
+}
+```
+
+}
+
 flutter {
-    source = "../.."
+
+```
+source = "../.."
+```
+
 }
